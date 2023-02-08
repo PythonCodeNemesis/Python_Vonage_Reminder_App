337781a62a8ec9717d6ed1ce1fa693739a27ae15
PythonCodeNemesis <durgaminakshmi@gmail.com>
2023-02-08 19:12:10 +0530

    init

---
+++ backend/app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from twilio.rest import Client
+import schedule
+import time
+import vonage
+from flask_apscheduler import APScheduler
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+scheduler = APScheduler()
+
+client = vonage.Client(key="key", secret="secret")
+sms = vonage.Sms(client)
+
+
+# A list to store phone numbers and frequency of reminders
+subscribers = []
+
+def send_reminders(phone_number):
+  
+  responseData = sms.send_message(
+  {
+      "from": "Vonage APIs",
+      "to": phone_number,
+      "text": "A text message sent using the Nexmo SMS API scheduler",
+  }
+  )
+
+
+  if responseData["messages"][0]["status"] == "0":
+      print("Message sent successfully.")
+  else:
+      print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+
+@app.route("/sign-up", methods=["POST"])
+@cross_origin()
+def sign_up():
+  data = request.get_json()
+  phone_number = data["phoneNumber"]
+  frequency = data["frequency"]
+  print(phone_number)
+
+  # Add the phone number and frequency to the subscribers list
+  subscribers.append({"phone_number": phone_number, "frequency": frequency})
+
+  responseData = sms.send_message(
+    {
+        "from": "Vonage APIs",
+        "to": phone_number,
+        "text": "A text message sent using the Nexmo SMS API first",
+    }
+  )
+
+  if responseData["messages"][0]["status"] == "0":
+      print("Message sent successfully.")
+  else:
+      print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+
+  scheduler.add_job(id = 'Scheduled Task', func=send_reminders, args = [phone_number], trigger="interval", minutes=int(frequency))
+  print("send messge to",phone_number,"with frequency",frequency)
+
+
+  return "{\"status\" : \"ok\"}", 200
+
+if __name__ == "__main__":
+  # Schedule the send_reminders function to run at the frequency specified by the user
+
+  
+  scheduler.start()
+  app.run(debug=True)
+
+
